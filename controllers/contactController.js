26ba48e68d671ba15b32666922397339ab6f0c3d
sanmo-yousof <sanmoyousof@gmail.com>
2025-12-07 18:22:46 +0600

    connected with mongodb

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,38 +1,44 @@
+const asyncHandler = require("express-async-handler")
+
 // @desc get all contacts
 // @route GEt /api/contacts
 // @access public 
-const getContacts = async(req,res) => {
+const getContacts = asyncHandler((req,res) => {
     res.status(200).json({message:"Get all contacts"});
-}
+})
+
 // @desc Create New Contact
 // @route POST /api/contacts
 // @access public 
-const postContacts = async(req,res) => {
+const postContacts = asyncHandler((req,res) => {
     const {name,email,phone} = req.body;
     if(!name || !email || !phone){
         res.status(400);
         throw new Error ("All fileds are required !")
     }
     res.status(201).json({message:"Create Contact"});
-}
+})
+
 // @desc Get Contacts
 // @route GET /api/contacts/:id
 // @access public 
-const getContact = async(req,res) => {
+const getContact = asyncHandler((req,res) => {
     res.status(200).json({message:`Get contact for ${req.params.id}`});
-}
+})
+
 // @desc Update Contacts
 // @route PUT /api/contacts/:id
 // @access public 
-const updateContact = async(req,res) => {
+const updateContact = asyncHandler((req,res) => {
     res.status(200).json({message:`Update contact for ${req.params.id}`});
-}
+})
+
 // @desc Get Contacts
 // @route DELETE /api/contacts/:id
 // @access public 
-const deleteContact = async(req,res) => {
+const deleteContact = asyncHandler((req,res) => {
     res.status(200).json({message:`Delete contact for ${req.params.id}`});
-}
+})
 
 module.exports = {
     getContact,
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
 require("dotenv").config()
 
 const app = express()
 const port = process.env.PORT || 5000;
 
-
+connectDB();
 app.use(express.json())
 app.use("/api/contacts",require("./routes/contactRouter"))
 app.use(errorHandler)
